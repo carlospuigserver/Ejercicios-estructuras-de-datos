83fb3e4a55ed8535e4b34c862d489091adebf2c2
carlospuigserver <cpuiggut@myuax.com>
2022-04-17 17:37:09 +0200

    ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,16 @@
+from asyncore import write
+
+
 class usuario:
     def __init__(self,linea1,linea2):
         self.linea1=linea1
         self.linea2=linea2
+        #mayúsculas
+        linea1.upper()
+        linea2.upper()
+
+def fichero(self):
+    archivo=open("xxxx.txt","w")
+    archivo.write(self.linea1)
+    archivo.write(self.linea2)
+    archivo.close()
